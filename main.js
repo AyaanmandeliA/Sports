0c0f852a8000925f8f978b303c8db47d9bb7dc00
AyaanmandeliA <78735976+AyaanmandeliA@users.noreply.github.com>
2022-05-23 17:06:20 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function about(){
+    window.location="about_the_team.html";
+}
+function sponsor(){
+    window.location="sponsor.html";
+}
+function project(){
+    window.location="project.html";
+}
+
+function reports(){
+    window.location="journals.html";
+   
+}
